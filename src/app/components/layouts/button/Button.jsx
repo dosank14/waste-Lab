3fc672f1952265e1c13bcffacc4ccdc6d6a07291
dosank14 <dosank14@gmail.com>
2024-12-17 18:30:59 +0900

    警告文をなくした

--- src/app/components/layouts/button/Button.jsx
+++ src/app/components/layouts/button/Button.jsx
@@ -7,25 +7,25 @@ import './button_blue.css';
 const Button = () => {
     return (
         <div>
-            <Link href={{ pathname: '/game', query: { type: 'japanese' } }}>
+            <Link href={{ pathname: '/game', query: { type: 'japanese' }}} scroll={false}>
                 <button className="button1" type="button">
                     中華
                 </button>
             </Link>
 
-            <Link href={{ pathname: '/game', query: { type: 'western' } }}>
+            <Link href={{ pathname: '/game', query: { type: 'western' } }} scroll={false}>
                 <button className="button2" type="button">
                     和食
                 </button>
             </Link>
 
-            <Link href={{ pathname: '/game', query: { type: 'chinese' } }}>
+            <Link href={{ pathname: '/game', query: { type: 'chinese' } }} scroll={false}>
                 <button className="button3" type="button">
                     洋食
                 </button>
             </Link>
 
-            <Link href={{ pathname: '/game', query: { type: 'random' } }}>
+            <Link href={{ pathname: '/game', query: { type: 'random' } }} scroll={false}>
                 <button className="button4" type="button">
                     ランダム
                 </button>
